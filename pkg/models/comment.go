924636e7de5e08d333ad107bc54e95d6775c1d4c
stepan103ua <stepan8877ua@gmail.com>
2022-12-06 15:12:33 +0200

    feat: add required fields

--- pkg/models/comment.go
+++ pkg/models/comment.go
@@ -2,7 +2,7 @@ package models
 
 type Comment struct {
 	Id      int    `json:"id" db:"id"`
-	PostId  int    `json:"post_id" db:"post_id"`
-	Comment string `json:"comment" db:"comment"`
+	PostId  int    `json:"post_id" db:"post_id" binding:"required"`
+	Comment string `json:"comment" db:"comment" binding:"required"`
 	OwnerId int    `json:"owner_id" db:"owner_id"`
 }
