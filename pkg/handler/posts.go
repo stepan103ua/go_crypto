f338935e22aeaeab332510aed3488c62796af50f
stepan103ua <stepan8877ua@gmail.com>
2022-12-06 19:05:06 +0200

    feat: add update post to handler

--- pkg/handler/posts.go
+++ pkg/handler/posts.go
@@ -12,6 +12,13 @@ type GetAllPostsResponse struct {
 	Data []models.Post `json:"posts"`
 }
 
+type UpdatePostRequest struct {
+	Title          string `json:"title" binding:"required"`
+	Description    string `json:"description" binding:"required"`
+	CryptoCurrency string `json:"crypto_currency" binding:"required"`
+	ImageUrl       string `json:"image_url" binding:"required"`
+}
+
 func (h *Handler) createPost(c *gin.Context) {
 	var input models.Post
 
@@ -131,5 +138,48 @@ func (h *Handler) deletePost(c *gin.Context) {
 
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "Successfully deleted",
+	})
+}
+
+func (h *Handler) updatePost(c *gin.Context) {
+	var input UpdatePostRequest
+
+	err := c.BindJSON(&input)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	postId := c.Param("postId")
+
+	if postId == "" {
+		newErrorResponse(c, http.StatusInternalServerError, "Wrong post id")
+		return
+	}
+
+	postIdInt, err := strconv.Atoi(postId)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, "Wrong post id")
+		return
+	}
+
+	userId, err := h.getUserId(c)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	err = h.service.UpdatePost(input.Title, input.Description, input.CryptoCurrency, input.ImageUrl, postIdInt, userId)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Successfully updated",
 	})
 }
