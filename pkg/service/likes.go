41a3d9a85c4e8e9320b322b86b43592341587ade
stepan103ua <stepan8877ua@gmail.com>
2022-12-07 16:32:40 +0200

    feat: add toggle like to service

---
+++ pkg/service/likes.go
@@ -0,0 +1,30 @@
+package service
+
+import "crypto_app/pkg/repository"
+
+type LikesService struct {
+	repository repository.Likes
+}
+
+func NewLikesService(repository repository.Likes) *LikesService {
+	return &LikesService{repository: repository}
+}
+
+func (s *LikesService) ToggleLike(postId, userId int) error {
+	isLiked, err := s.repository.IsLiked(postId, userId)
+	if err != nil {
+		return err
+	}
+
+	if isLiked {
+		err = s.repository.Dislike(postId, userId)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	err = s.repository.PutLike(postId, userId)
+
+	return err
+}
