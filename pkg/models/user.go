533864d50b31b488929e39742d02899dca7b6937
stepan103ua <stepan8877ua@gmail.com>
2022-12-08 19:44:58 +0200

    feat: add about to user model

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -5,16 +5,19 @@ type User struct {
 	Name     string `json:"name" db:"name" binding:"required"`
 	Email    string `json:"email" db:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
+	About    string `json:"about" default0:""`
 }
 
 type UserResponse struct {
 	Id    int    `json:"id" db:"id"`
 	Name  string `json:"name" db:"name"`
 	Email string `json:"email" db:"email"`
+	About string `json:"about" db:"about"`
 }
 
 type UserUpdate struct {
 	Name     string `json:"name" db:"name"`
 	Email    string `json:"email" db:"email"`
 	Password string `json:"password" db:"password"`
+	About    string `json:"about" db:"about"`
 }
